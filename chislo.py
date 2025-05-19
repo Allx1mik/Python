f40c5aadd1cdc0a0de0d20a5e064f7fe5d681185
Allx1mik <mrforbin@mail.ru>
2025-05-20 02:32:26 +0300

    Add files via upload

---
+++ chislo.py
@@ -0,0 +1,15 @@
+print("Андреев В.К. группа 1408-05\n")
+
+while True:
+    user_input = input("Введите число: ")
+    # Проверяем, состоит ли ввод только из цифр
+    if user_input.isdigit():
+        number = int(user_input)
+        # Проверяем чётность
+        if number % 2 == 0:
+            print(f"Число {number} является четным")
+        else:
+            print(f"Число {number} не является четным")
+        break
+    else:
+        print("Ошибка: введено не число")
